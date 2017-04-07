abc6290042a7f68d7a00edc02eb53d9568b2ac50
Ruben Grimm <ruben.grimm@zweitag.de>
2017-04-07 12:25:58 +0200

    Remove unused step definition

--- features/support/middleman_images_steps.rb
+++ features/support/middleman_images_steps.rb
@@ -5,15 +5,6 @@ Then(/^the dimensions of the file "([^"]*)" should be (\d+)x(\d+)$/) do |path, x
   expect(image.dimensions).to eql [x.to_i, y.to_i]
 end
 
-Then(/^the exif data of the file "([^"]*)" should( not)? be empty$/) do |path, negated|
-  image = MiniMagick::Image.open expand_path(path)
-  if negated
-    expect(image.exif).to_not be_empty
-  else
-    expect(image.exif).to be_empty
-  end
-end
-
 Then(/^the file "([^"]*)" should be smaller than the file "([^"]*)"/) do |file1, file2|
   expect(File.size(expand_path(file1)) < File.size(expand_path(file2))).to be_truthy
 end
